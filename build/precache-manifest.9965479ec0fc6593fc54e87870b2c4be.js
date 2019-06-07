9f1beb2eb71cad4e39247d651a055737b5f51247
Kritika Umashankar <kritikaumashankar@gmail.com>
2019-06-06 19:44:16 -0600

    changed gitignore

---
+++ build/precache-manifest.9965479ec0fc6593fc54e87870b2c4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56688b9d13ae13724ab4",
+    "url": "/static/js/main.56688b9d.chunk.js"
+  },
+  {
+    "revision": "addf7a5117164d3fe128",
+    "url": "/static/js/1.addf7a51.chunk.js"
+  },
+  {
+    "revision": "56688b9d13ae13724ab4",
+    "url": "/static/css/main.6e8a9234.chunk.css"
+  },
+  {
+    "revision": "addf7a5117164d3fe128",
+    "url": "/static/css/1.e98d92ab.chunk.css"
+  },
+  {
+    "revision": "26aa9a09f1aed71de902721840ed45f2",
+    "url": "/index.html"
+  }
+];
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { 
   Card,
-  CardGroup,
   CardImg,
   CardBody,
   CardTitle,

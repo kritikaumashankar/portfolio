51d2ebdfd92fcf739cab8044b2fccf282a52f9f8
Kritika Umashankar <kritikaumashankar@gmail.com>
2019-08-01 14:18:42 -0600

    Redesigned the website

---
+++ build/precache-manifest.a3e5c34d342ab010de04c6d9c99accce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cb0167dcae55127d204",
+    "url": "/static/js/main.6cb0167d.chunk.js"
+  },
+  {
+    "revision": "56bd8e1d20967da25e26",
+    "url": "/static/js/1.56bd8e1d.chunk.js"
+  },
+  {
+    "revision": "6cb0167dcae55127d204",
+    "url": "/static/css/main.4e9a7e1c.chunk.css"
+  },
+  {
+    "revision": "56bd8e1d20967da25e26",
+    "url": "/static/css/1.156f784d.chunk.css"
+  },
+  {
+    "revision": "fc793cb4260699f8cdd573144f5ba7cd",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7ca923a34d043df70677d51452c41d3.js"
+  "/precache-manifest.a3e5c34d342ab010de04c6d9c99accce.js"
 );
 
 workbox.clientsClaim();
